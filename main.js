9ae715dcf8261f2f07ec67f6b27fc830ca8c9640
pjn51 <58340957+pjn51@users.noreply.github.com>
2023-05-04 20:40:03 -0700

    small update

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-function reverseString(str) {
-    var reversed = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-        reversed += str[i];
-    }
-    return reversed;
-}
-var reversed = reverseString("Snazzy");
-console.log(reversed);
+"use strict";
+let age = 20;
+if (age < 50)
+    age += 10;
+//# sourceMappingURL=main.js.map
