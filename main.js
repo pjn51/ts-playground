aadd33e123abce481a113900704eac7eb3365f8f
pjn51 <58340957+pjn51@users.noreply.github.com>
2023-05-06 18:45:58 -0700

    banking app

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-"use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-//# sourceMappingURL=main.js.map
+var Account = /** @class */ (function () {
+    function Account(id, initialBalance) {
+        this.id = id;
+        this.balance = initialBalance;
+        console.log("Created new account with initial balance $".concat(initialBalance));
+    }
+    Account.prototype.deposit = function (amount) {
+        this.balance += amount;
+        console.log("Desposited $".concat(amount, ". New balance is $").concat(this.balance, "."));
+    };
+    Account.prototype.withdraw = function (amount) {
+        if (amount > this.balance) {
+            throw new Error('Insufficient balance');
+        }
+        console.log("Withdrew $".concat(amount, ". New balance is $").concat(this.balance, "."));
+        this.balance -= amount;
+    };
+    return Account;
+}());
+function transfer(fromAccount, toAccount, amount) {
+    fromAccount.withdraw(amount);
+    toAccount.deposit(amount);
+}
+// Example usage:
+var account1 = new Account(1, 1000);
+var account2 = new Account(2, 500);
+console.log('Initial balances:');
+console.log("Account 1: ".concat(account1.balance));
+console.log("Account 2: ".concat(account2.balance));
+account1.deposit(200);
+account1.withdraw(100);
+transfer(account1, account2, 300);
+console.log('Balances after operations:');
+console.log("Account 1: ".concat(account1.balance));
+console.log("Account 2: ".concat(account2.balance));
--- main.ts
+++ main.ts
@@ -1,3 +1,45 @@
-let age: number = 20;
-if (age < 50)
-  age += 10;
+class Account {
+  id: number;
+  balance: number;
+
+  constructor(id: number, initialBalance: number) {
+    this.id = id;
+    this.balance = initialBalance;
+    console.log(`Created new account with initial balance $${initialBalance}`)
+  }
+
+  deposit(amount: number): void {
+    this.balance += amount;
+    console.log(`Desposited $${amount}. New balance is \$${this.balance}.`)
+  }
+
+  withdraw(amount: number): void {
+    if (amount > this.balance) {
+      throw new Error('Insufficient balance');
+    }
+    console.log(`Withdrew $${amount}. New balance is $${this.balance}.`)
+    this.balance -= amount;
+  }
+}
+
+function transfer(fromAccount: Account, toAccount: Account, amount: number): void {
+  fromAccount.withdraw(amount);
+  toAccount.deposit(amount);
+}
+
+// Example usage:
+
+const account1 = new Account(1, 1000);
+const account2 = new Account(2, 500);
+
+console.log('Initial balances:');
+console.log(`Account 1: ${account1.balance}`);
+console.log(`Account 2: ${account2.balance}`);
+
+account1.deposit(200);
+account1.withdraw(100);
+transfer(account1, account2, 300);
+
+console.log('Balances after operations:');
+console.log(`Account 1: ${account1.balance}`);
+console.log(`Account 2: ${account2.balance}`);
